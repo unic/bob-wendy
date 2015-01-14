e3560458d25460db0e83974ad08b1d353c636740
Yannis Guedel <yannis.guedel@unic.com>
2015-01-14 11:06:58 +0100

    fixed typos and clarified sentence

--- src/tools/BobConfig/Get-ScProjectPath.ps1
+++ src/tools/BobConfig/Get-ScProjectPath.ps1
@@ -6,15 +6,14 @@ Gets the full path of the Visual Studio Website project with the current context
 There are 3 possibilities how the path can be calculated:
 * It is provided as parameter.
 * If the current Visual Studio project contains a Bob.config file, the path of the project will be returned.
-* If the current solution continas a *.Website project, the path of the *.Website project will be returned.
+* If the current solution contains a *.Website project, the path of the *.Website project will be returned.
 
 .PARAMETER ProjectPath
 If ProjectPath  is provided, it will be returned.
 This enables consumers of Get-ScProjectPath to just forward their ProjectPath-parameter to Get-ScProjectPath without checking if it was provided.
 
 .PARAMETER ConfigFilePath
-The path inside a project where the config files are.
-
+The folder containing the Bob configuration files, relative to the location of the .csproj
 .PARAMETER ConfigFileName
 A list of names for config files
 
