bfe5a4b27e57fe85801a7a00bac7d862b71fa45d
Yannis Guedel <yannis.guedel@unic.com>
2015-04-02 09:58:01 +0200

    fixed path to bob.config.user

--- src/tools/init.ps1
+++ src/tools/init.ps1
@@ -6,5 +6,5 @@ $projectPath = Get-ScProjectPath
 
 $bobConfigUser = "$projectPath\App_Config\Bob.config.user"
 if(-not (Test-Path $bobConfigUser)) {
-    cp "$toolsPath\..\content\App_Config\Bob.config.user" $bobConfigUser
+    cp "$installPath\content\App_Config\Bob.config.user" $bobConfigUser
 }
