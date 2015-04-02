7c2c2e4fca9365709bb1d31c08bcef0610aa874a
Yannis Guedel <yannis.guedel@unic.com>
2015-04-02 09:22:23 +0200

    added create bob.config.user to init

--- src/tools/init.ps1
+++ src/tools/init.ps1
@@ -1,3 +1,10 @@
 param($installPath, $toolsPath, $package)
 
 Import-Module (Join-Path $toolsPath "BobConfig")
+
+$projectPath = Get-ScProjectPath
+
+$bobConfigUser = "$projectPath\App_Config\Bob.config.user"
+if(-not (Test-Path $bobConfigUser)) {
+    cp "$toolsPath\..\content\App_Config\Bob.config.user" $bobConfigUser
+}
