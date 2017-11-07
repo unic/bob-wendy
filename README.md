497e1ac82094a6da69cc789906496e0ef62ff2cf
Reto Hugi <retohugi@gmx.ch>
2017-11-07 16:17:47 +0100

    initial oss commit

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
+![Build](https://ci.appveyor.com/api/projects/status/github/unic/bob-wendy?svg=true)
+
 # Wendy
 
 Wendy is responsible for configuring Bob and providing APIs to access the configuration.
 
-### Build
-[![Build](https://teamcity.unic.com/httpAuth/app/rest/builds/buildType:Sitecore_Frameworks_Bob_Config_Build/statusIcon)](https://teamcity.unic.com/viewType.html?buildTypeId=Sitecore_Frameworks_Bob_Config_Build)
-
 ## Documentation
 
-<https://teamcity.unic.com/repository/download/Sitecore_Frameworks_Bob_TheBook_BuildTheBook/.lastSuccessful/Wendy/README.html>
+See [latest documentation](https://unic.github.io/bob-wendy)
--- generateDocs.ps1
+++ generateDocs.ps1
@@ -1,17 +1,10 @@
-﻿param($username, $password, [switch]$Buildserver)
+﻿$PSScriptRoot = split-path -parent $MyInvocation.MyCommand.Definition
 
-$PSScriptRoot = split-path -parent $MyInvocation.MyCommand.Definition
-
-$module = "BobConfig"
+$module = "Wendy"
 
 Import-Module "$PSScriptRoot\packages\Unic.Bob.Keith\Keith"
-Import-Module "$PSScriptRoot\src\tools\BobConfig" -Force
+Import-Module "$PSScriptRoot\src\tools\$module" -Force
 
 New-PsDoc -Module $module -Path "$PSScriptRoot\docs\" -OutputLocation "$PSScriptRoot\docs-generated"
 
-New-PsDoc -Module $module -Path "$PSScriptRoot\docs\" -OutputLocation "$PSScriptRoot\docs-generated" -Verbose
-New-GitBook "$PSScriptRoot\docs-generated" "$PSScriptRoot\temp" $username $password -Buildserver:$Buildserver
-
-
-
-
+New-GitBook "$PSScriptRoot\docs-generated" "$PSScriptRoot\temp"
